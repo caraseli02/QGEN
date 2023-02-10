4a94dfd009b33dfe93503b41f2473a1dcd8a30b9
vlad <caraseli02@gmail.com>
2023-02-11 00:02:38 +0100

    feat: finish the datatable and dogs card views

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,40 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { useDark } from '@vueuse/core'
+
+const isDark = useDark()
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <RouterView v-slot="{ Component }">
+    <template v-if="Component">
+      <Transition mode="out-in">
+        <Suspense>
+          <!-- main content -->
+          <component class="mx-24 mt-24" :is="Component"></component>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+          <!-- loading state -->
+          <template #fallback>
+            <el-skeleton style="width: 240px">
+              <template #template>
+                <el-skeleton-item variant="image" style="width: 240px; height: 240px" />
+                <div style="padding: 14px">
+                  <el-skeleton-item variant="p" style="width: 50%" />
+                  <div style="
+                              display: flex;
+                              align-items: center;
+                              justify-items: space-between;
+                            ">
+                    <el-skeleton-item variant="text" style="margin-right: 16px" />
+                    <el-skeleton-item variant="text" style="width: 30%" />
+                  </div>
+                </div>
+              </template>
+            </el-skeleton>
+          </template>
+        </Suspense>
+      </Transition>
+    </template>
+  </RouterView>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,26 @@
 <script setup lang="ts">
+import type { IDogsList } from '@/types';
+import { useFetch } from '@vueuse/core'
+import { computed } from 'vue';
+
+const url = 'https://api.thedogapi.com/v1/images/search?limit=11&has_breeds=1&api_key=live_M7F5gZDNmkcVJs91ZMc1XPOF00M9GbkCtY0c0wgpN9iKNJ4L8me1xnS2mzHkNK2D'
+
+const { isFetching, error, data } = await useFetch(url)
+
+const DogsList = computed<IDogsList[]>(() => {
+  try {
+    return JSON.parse(data.value as string)
+  }
+  catch (e) {
+    return null
+  }
+})
+
 </script>
 
 <template>
   <main>
-    Home Page
+    <DataTable :DogsList="DogsList" />
+    <DogCard v-for="dog in DogsList" :dogData="dog" :key="dog.id" />
   </main>
 </template>
