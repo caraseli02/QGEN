992f6ff6cfc19d79aa90dfc77ce3022feea79b7f
vlad <caraseli02@gmail.com>
2023-02-13 12:50:12 +0100

    feat: change the skeleton loader

--- src/App.vue
+++ src/App.vue
@@ -16,22 +16,9 @@ useDark()
 
           <!-- loading state -->
           <template #fallback>
-            <el-skeleton style="width: 240px">
-              <template #template>
-                <el-skeleton-item variant="image" style="width: 240px; height: 240px" />
-                <div style="padding: 14px">
-                  <el-skeleton-item variant="p" style="width: 50%" />
-                  <div style="
-                              display: flex;
-                              align-items: center;
-                              justify-items: space-between;
-                            ">
-                    <el-skeleton-item variant="text" style="margin-right: 16px" />
-                    <el-skeleton-item variant="text" style="width: 30%" />
-                  </div>
-                </div>
-              </template>
-            </el-skeleton>
+            <div class="w-[95vw] h-[95vh] flex justify-center items-center">
+              <el-skeleton :rows="5" animated />
+            </div>
           </template>
         </Suspense>
       </Transition>
